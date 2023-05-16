0a6fe7aec2267a4aed7732b06dce90bd2b7812d1
jeonbyeongmin <qudals7613@gmail.com>
2023-05-16 17:56:15 +0900

    fix(demo): demo resources path

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/use-table-drag-select/',
   plugins: [react()],
   build: {
     outDir: '../docs',
