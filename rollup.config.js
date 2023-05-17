ea3e4ab0e2c26776f10953a51cfaf5941198e9bb
jeonbyeongmin <qudals7613@gmail.com>
2023-05-18 01:05:00 +0900

    fix(rollup): update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,33 @@
-const { generateRollupConfig } = require('./configs/rollup');
+const babel = require('@rollup/plugin-babel').default;
+const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json');
+const resolve = require('@rollup/plugin-node-resolve').default;
 
-module.exports = generateRollupConfig({
-  packageDir: __dirname,
-});
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+module.exports = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/bundle.esm.js',
+      format: 'esm',
+    },
+    {
+      file: 'dist/bundle.cjs.js',
+      format: 'cjs',
+    },
+  ],
+  plugins: [
+    resolve({
+      extensions,
+    }),
+    commonjs(),
+    babel({
+      extensions,
+      babelHelpers: 'bundled',
+      rootMode: 'upward',
+    }),
+    json(),
+  ],
+  external: ['react', 'react-dom'],
+};
